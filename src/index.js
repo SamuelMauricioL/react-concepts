49a7a66449cf867e635b15a916684e439a6ccbfb
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-02-14 17:30:07 -0500

    props and props deconstructing

--- src/index.js
+++ src/index.js
@@ -1,37 +1,55 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-// stateless functional component
-// always return JSX
+// CSS
+import "./index.css";
 
-function Greetings() {
+//setup vars
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/515mvgd0pXL._SX329_BO1,204,203,200_.jpg",
+  title:
+    "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+  author: "James Clear",
+};
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/41n9-p6-PpL._SX331_BO1,204,203,200_.jpg",
+  title: "Reminders of Him: A Novel",
+  author: "Colleen Hoover",
+};
+
+function BookList() {
   return (
-    <>
-      <div className="">
-        <h3>hello people</h3>
-        <ul>
-          <li>
-            <a href="#">Hi</a>
-          </li>
-          <img src="" alt="" />
-          <input type="text" name="" id="" />
-        </ul>
-      </div>
-      <div>
-        <Person />
-        <Message />
-      </div>
-    </>
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam</p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
   );
 }
 
-const Person = () => <h2>John doe</h2>;
-
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = ({ img, title, author, child }) => {
+  // const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {child}
+    </article>
+  );
 };
 
-// const Greetings = () =>
+// const BookList = () =>
 //   React.createElement("div", {}, React.createElement("h1", {}, "hello world"));
 
-ReactDom.render(<Greetings />, document.getElementById("root"));
+ReactDom.render(<BookList />, document.getElementById("root"));
