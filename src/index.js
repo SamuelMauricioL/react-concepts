65b9ec0910fc6bca807322143c43d0f9b2b11d5e
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-02-14 18:07:47 -0500

    Event basic

--- src/index.js
+++ src/index.js
@@ -31,14 +31,26 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  const { img, title, author } = props;
+const Book = ({ img, title, author }) => {
+  const clickHandler = () => {
+    alert(`${title} by ${author}`);
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
+
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
+      <button type="button" onClick={clickHandler}>
+        Reference example
+      </button>
+      <button type="button" onClick={() => complexExample(author)}>
+        More complex example
+      </button>
     </article>
   );
 };
