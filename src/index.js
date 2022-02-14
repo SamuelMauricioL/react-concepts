dccae3732f2d080a947f554bbfff6cc798e293a8
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-02-14 16:07:29 -0500

    hi

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+
+// stateless functional component
+// always return JSX
 
 function Greetings() {
-  return <h4>I'm Samuel and this is my firts components</h4>;
+  return (
+    <>
+      <div className="">
+        <h3>hello people</h3>
+        <ul>
+          <li>
+            <a href="#">Hi</a>
+          </li>
+          <img src="" alt="" />
+          <input type="text" name="" id="" />
+        </ul>
+      </div>
+      <div>
+        <Person />
+        <Message />
+      </div>
+    </>
+  );
 }
 
-ReactDom.render(<Greetings />, document.getElementById('root'));
+const Person = () => <h2>John doe</h2>;
+
+const Message = () => {
+  return <p>this is my message</p>;
+};
+
+// const Greetings = () =>
+//   React.createElement("div", {}, React.createElement("h1", {}, "hello world"));
+
+ReactDom.render(<Greetings />, document.getElementById("root"));
